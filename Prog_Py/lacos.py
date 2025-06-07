ef26e2e0819977950eb44eaed1461f56bcb81759
Gustavo Berger <126837544+IceBerger-Dev@users.noreply.github.com>
2025-06-07 00:49:45 +0000

    For

---
+++ Prog_Py/lacos.py
@@ -0,0 +1,30 @@
+# import random
+# frutas = ['maçã', 'banana', 'uva', 'abacaxi']
+
+# escolha =  random.choice(frutas)
+
+# separa = list(escolha)
+
+# for index, letra in enumerate(separa):
+    
+#     print(f'A letra é {letra}, já seu index é {index}')
+
+
+# listaSalario = {}
+# decimo = {}
+
+# nome = str.capitalize(input('Digite seu nome: '))
+# salarioAnual = float(input('Digite seu salario anual: '))
+# salario = round((salarioAnual/12),2)
+
+# listaSalario[nome] = salarioAnual
+# decimo[salarioAnual] = salario
+
+# print(listaSalario)
+# print(decimo)
+
+quadrados = [x for x in range(0,500,20)]
+
+print(quadrados)
+
+
